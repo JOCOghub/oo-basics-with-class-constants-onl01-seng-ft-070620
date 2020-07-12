7b13175561158573c32f53ef8e7967017eeb972c
Joseph cohen <jojocohen57@gmail.com>
2020-07-12 21:40:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,20 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader  :title, :brand
 
-  def initialize(brand)
-    @brand = brand
+  BRANDS = [].uniq
+  
+  def initialize(title)
+    @title = title
   end
 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand 
+  end
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
